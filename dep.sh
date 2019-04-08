a31a659a9fa36e5028a123447aba3292c1402a78
james-5 <jamesthurman473@gmail.com>
2019-04-08 09:48:51 -0500

    Add files via upload

---
+++ dep.sh
@@ -0,0 +1,10 @@
+#bin/bash!
+sudo git clone https://github.com/hansonrobotics/ros_emopy.git
+cd ros_emopy 
+mkdir make
+cd make 
+sudo cmake ...
+cd
+sudo apt install libatlas-base-dev
+pip3 install tensorflow
+git clone https://github.com/tensorflow/tensorflow.git
